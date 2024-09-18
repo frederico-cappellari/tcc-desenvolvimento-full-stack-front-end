b33b1bab13bca07ddff371eb33bbe4e4d792f9f6
Gustavo Gomes De Medeiros <gustavo-medeiros@PROCERGS.REDERS>
2024-09-18 09:09:06 -0300

    feat: altera url de http para https

--- src/environments/environment.hml.ts
+++ src/environments/environment.hml.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: false,
   // Configurações Aplicação:
-  clientUrl: 'http://apm02.des.intra.rs.gov.br',
-  apiUrl: 'http://apm02.des.intra.rs.gov.br',
+  clientUrl: 'https://apm02.des.intra.rs.gov.br',
+  apiUrl: 'https://apm02.des.intra.rs.gov.br',
 
   soeauth: {
     authority: 'https://www.soe.rs.gov.br/soeauth-hml/.well-known/openid-configuration',
     clientId: 'apm.i2.des.5Q2MC0k5pwA5Vy1JNfV',
-    redirectUrl: "http://apm02.des.intra.rs.gov.br",
-    postLogoutRedirectUri: "http://apm02.des.intra.rs.gov.br",
+    redirectUrl: "https://apm02.des.intra.rs.gov.br",
+    postLogoutRedirectUri: "https://apm02.des.intra.rs.gov.br",
     scope: 'openid',
     responseType: 'code',
     silentRenew: true,
