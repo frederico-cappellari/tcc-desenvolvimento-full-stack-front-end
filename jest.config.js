4a1e6428b9f3f95d61f86f050f70ad18cc8f1cc7
aline-silva <aline-silva@PROCERGS.REDERS>
2024-11-04 16:28:49 -0300

    feat: configurando cobertura de codigo com cypress

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   preset: 'jest-preset-angular',
   globalSetup: 'jest-preset-angular/global-setup',
+  collectCoverage: true,
+  coverageDirectory: 'jest-coverage',
   moduleNameMapper: {
     '^ng2-charts$': '<rootDir>/src/__mocks__/ng2-charts.mock.ts', // Mock do ng2-charts
   },
