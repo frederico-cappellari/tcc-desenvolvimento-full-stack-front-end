1c1175f998aee922ffad1e663874f6f32d3ca770
Gustavo Gomes De Medeiros <gustavo-medeiros@PROCERGS.REDERS>
2024-12-12 09:22:42 -0300

    feat: altera environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,15 @@
 export const environment = {
   production: true,
   // Configurações Aplicação:
-  clientUrl: 'https://apm02.des.intra.rs.gov.br',
+  clientUrl: 'https://apm03.hml.intra.rs.gov.br',
   apiUrl: 'https://apm-quarkus.apps.kildes4830.des.intra.rs.gov.br',
 
   soeauth: {
     configId: 'soeAuthConfig',
     authority: 'https://soe.intra.rs.gov.br/soeauth-des/.well-known/openid-configuration',
     clientId: 'apm.i2.des.5Q2MC0k5pwA5Vy1JNfV',
-    redirectUrl: "https://apm02.des.intra.rs.gov.br",
-    postLogoutRedirectUri: "https://apm02.des.intra.rs.gov.br",
+    redirectUrl: "https://apm03.hml.intra.rs.gov.br",
+    postLogoutRedirectUri: "https://apm03.hml.intra.rs.gov.br",
     scope: 'openid',
     responseType: 'code',
     silentRenew: true,
@@ -22,8 +22,8 @@ export const environment = {
     clientId: '530_re15h32dv7kws04s044gwo8o40kc4g8gw0kcsgo48cw4cossk',
     scope: 'openid',
     responseType: 'code',
-    redirectUrl: "https://apm02.des.intra.rs.gov.br",
-    postLogoutRedirectUri: "https://apm02.des.intra.rs.gov.br/login",
+    redirectUrl: "https://apm03.hml.intra.rs.gov.br",
+    postLogoutRedirectUri: "https://apm03.hml.intra.rs.gov.br/login",
   },
   mapsKey: 'AIzaSyAsPR982x3S-7a8SSHJzgIiUrnWU1c82X0',
 };
